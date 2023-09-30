e9f9a0e334e746afe55889af7644a6b87c19898c
alexander.nikiforov <nikiforov.san.sanich@gmail.com>
2023-09-30 22:13:08 +0400

    11. Add cors

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ require('dotenv').config();
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const colors = require('colors');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 connectDB()
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
